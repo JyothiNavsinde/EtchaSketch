0444e337774883b6302ef0cb834c8b83e70c0cb8
Jyothi Navsinde <jyothinavsinde@gmail.com>
2025-06-14 15:25:24 +0530

    created a container and then using css gave width and height of the container later used a function to create a nxn grid and dynamically created a grid fixing the size of the container added a button which helps you create the new sketch pad erasing the last one

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 const gridCont = document.querySelector(".gridContainer");
-for(let i=0; i<16; i++) {
-    for(let j=0; j<16; j++) {
-        const divCell = document.createElement("div");
-        divCell.classList.add("gridCell");
-        gridCont.appendChild(divCell);
+
+const btn = document.querySelector("#newGrid");
+
+function createGrid(numSquares) {
+    gridCont.innerHTML = ""; //to clear the contianer
+    const sqaureSize = 1000/numSquares;
+    
+    for(let i=0; i<numSquares*numSquares; i++) {
+        const gridDiv = document.createElement("div");
+        gridDiv.className = "gridCell";
+        gridDiv.style.width = `${sqaureSize}px`;
+        gridDiv.style.height = `${sqaureSize}px`;
+
+        gridDiv.addEventListener("mouseover", () => {
+            gridDiv.style.backgroundColor = "purple";
+        });
+
+        gridCont.appendChild(gridDiv);
+
     }
 }
+
+createGrid(16);
+
+btn.addEventListener("click", () => {
+    let numSquares = prompt("How many squares per side?");
+
+    createGrid(numSquares);
+})
+
+
